61d15e8122b607ab595b731ba5dcb2c6036eb9d4
Syed Sarosh Farrukh <sarosh@dynamiconlinetechnologies.com>
2022-02-01 01:41:32 -0800

    First Commit

--- App.js
+++ App.js
@@ -4,7 +4,22 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import DrawerNav from "./app/navigation/DrawerNav";
 import StackNav from "./app/navigation/StackNav";
 
+// To see all the requests in the chrome Dev tools in the network tab.
+// XMLHttpRequest = GLOBAL.originalXMLHttpRequest
+//   ? GLOBAL.originalXMLHttpRequest
+//   : GLOBAL.XMLHttpRequest;
 
+// // fetch logger
+// global._fetch = fetch;
+// global.fetch = function (uri, options, ...args) {
+//   return global._fetch(uri, options, ...args).then((response) => {
+//     console.log("Fetch", { request: { uri, options, ...args }, response });
+//     return response;
+//   });
+// };
+
+//With current navigation layout, button navigation doesn't work properly
+//  since there is only one stack
 export default function App() {
   return (
     <SafeAreaProvider>
