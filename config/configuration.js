015c51573e7d58faa3e6d63f055d2d1362a2666a
aqibjaved-tech <58594872+aqibjaved-tech@users.noreply.github.com>
2023-06-16 20:54:44 +0500

    Update configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 class Configuration {
   // apiBaseUrl = "https://screening-app-backend-bn654.ondigitalocean.app/";
-  apiBaseUrl = "http://localhost:8080/";
+  apiBaseUrl = "https://victorious-jade-getup.cyclic.app/";
 }
 
 export default Configuration;
