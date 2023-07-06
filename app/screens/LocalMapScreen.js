8012e15d05c21abd9993cfd7218c616a507752bd
aqibjaved-tech <58594872+aqibjaved-tech@users.noreply.github.com>
2023-07-06 21:20:48 +0500

    map code
    run npm install react-native-maps

--- app/screens/LocalMapScreen.js
+++ app/screens/LocalMapScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import ZoomImage from '../components/ZoomImage';
 import colors from "../config/colors";
+import MapView, { Marker } from 'react-native-maps';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,14 +17,29 @@ const styles = StyleSheet.create({
     width: 1580 / 5,
   },
 });
-
+const mapStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
 const map = require("../assets/localmap.jpg");
 
 //hidden BottomTabBar
 function LocalMapScreen(props) {
+  const initialRegion = {
+    latitude: 32.797853866678864,
+    longitude: -117.15037831346157,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
   return (
     <View style={styles.container}>
-      <ZoomImage style={styles.image} source={map} />
+      <MapView style={mapStyles.map} initialRegion={initialRegion}>
+        <Marker coordinate={initialRegion} />
+      </MapView>
     </View>
   );
 }
